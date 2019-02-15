83c157f70dff2262ee00d49b03ccbf840d6f7bf8
Scott Macfarlane <smacfarlane@chef.io>
2019-02-15 10:36:23 -0800

    Add build/publish of Windows Docker Studio to release pipeline
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

---
+++ .buildkite/scripts/publish_docker_studio.ps1
@@ -0,0 +1,31 @@
+#!/usr/bin/env powershell
+
+#Requires -Version 5
+
+$ErrorActionPreference="stop" 
+
+$ReleaseChannel = & buildkite-agent meta-data get release-channel
+$isFakeRelease = & buildkite-agent meta-data exists fake-release
+
+Write-Host "--- Building the Windows Docker Studio"
+.buildkite/scripts/build_docker-image.ps1 -ReleaseChannel=$ReleaseChannel
+
+Write-Host "--- Publishing the Windows Docker Studio"
+. ./results/last_image.ps1
+Write-Host "Logging in to Bintray Docker repo"
+docker login -u="$env:BINTRAY_USER" -p="$env:BINTRAY_KEY" habitat-docker-registry.bintray.io
+
+if ! $isFakeRelease  {
+  Write-Host "I would publish docker images now. Don't want to try to build until I have this logic and code correct"
+  # try {
+    # Write-Host "Pushing ${docker_image}:$docker_image_version"
+    # docker push "${docker_image}:$docker_image_version"
+    # Write-Host "Pushing ${docker_image}:$docker_image_short_version tag for $docker_image_version"
+    # docker push "${docker_image}:$docker_image_short_version"
+    # Write-Host "Pushing latest tag for $docker_image_version"
+    # docker push "${docker_image}:latest"
+  # }
+  # finally {
+      # Remove-Item $HOME/.docker/config.json -Recurse -Force -ErrorAction SilentlyContinue
+  # }
+}
