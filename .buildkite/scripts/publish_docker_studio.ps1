caa7b8ed1f410214b5cb696d2be63c9e6b4cc288
Scott Macfarlane <smacfarlane@chef.io>
2019-02-15 17:07:10 -0800

    Powershell
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .buildkite/scripts/publish_docker_studio.ps1
+++ .buildkite/scripts/publish_docker_studio.ps1
@@ -5,12 +5,11 @@
 $ErrorActionPreference="stop" 
 $ReleaseChannel = & buildkite-agent meta-data get release-channel
 
-function isFakeRelease {
-  & buildkite-agent meta-data exists fake-release
-}
-
 Write-Host "--- Building the Windows Docker Studio"
 Write-Host "$ReleaseChannel"
+if(-Not(& buildkite-agent meta-data exists fake-release).ExitCode) {
+  Write-Host "FakeRelease"
+}
 
 & $PSScriptRoot/build_docker_image.ps1 -ReleaseChannel=$ReleaseChannel
 
@@ -19,7 +18,7 @@ Write-Host "--- Publishing the Windows Docker Studio"
 Write-Host "Logging in to Bintray Docker repo"
 docker login -u="$env:BINTRAY_USER" -p="$env:BINTRAY_KEY" habitat-docker-registry.bintray.io
 
-if(-Not(isFakeRelease)) {
+if(-Not(& buildkite-agent meta-data exists fake-release).ExitCode) {
   Write-Host "I would publish docker images now. Don't want to try to build until I have this logic and code correct"
   # try {
     # Write-Host "Pushing ${docker_image}:$docker_image_version"
