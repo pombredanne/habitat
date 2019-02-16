9bd4942f7c124bc620c35814360ffc905557b3a9
Scott Macfarlane <smacfarlane@chef.io>
2019-02-15 16:40:00 -0800

    Powershell
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- .buildkite/scripts/publish_docker_studio.ps1
+++ .buildkite/scripts/publish_docker_studio.ps1
@@ -3,14 +3,14 @@
 #Requires -Version 5
 
 $ErrorActionPreference="stop" 
-
 $ReleaseChannel = & buildkite-agent meta-data get release-channel
-$isFakeRelease = & buildkite-agent meta-data exists fake-release
+
+function isFakeRelease() {
+  & buildkite-agent meta-data exists fake-release
+}
 
 Write-Host "--- Building the Windows Docker Studio"
 Write-Host "$ReleaseChannel"
-Write-Host "$isFakeRelease"
-Write-Host $isFakeRelease.GetType()
 
 & $PSScriptRoot/build_docker_image.ps1 -ReleaseChannel=$ReleaseChannel
 
@@ -19,7 +19,7 @@ Write-Host "--- Publishing the Windows Docker Studio"
 Write-Host "Logging in to Bintray Docker repo"
 docker login -u="$env:BINTRAY_USER" -p="$env:BINTRAY_KEY" habitat-docker-registry.bintray.io
 
-if(-Not($isFakeRelease)) {
+if(-Not(isFakeRelease())) {
   Write-Host "I would publish docker images now. Don't want to try to build until I have this logic and code correct"
   # try {
     # Write-Host "Pushing ${docker_image}:$docker_image_version"
@@ -32,4 +32,7 @@ if(-Not($isFakeRelease)) {
   # finally {
       # Remove-Item $HOME/.docker/config.json -Recurse -Force -ErrorAction SilentlyContinue
   # }
+} else {
+  Write-Host "This is a fake release."
+  Write-Host "Not uploading $docker_image"
 }
